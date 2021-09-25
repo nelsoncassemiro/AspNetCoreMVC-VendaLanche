38049ce9001d39d0d87700bcaea97e038c91135a
nelson de sousa cassemiro <nelson.cassemiro@3coracoes.com.br>
2021-09-25 15:19:50 -0300

    Iniciando

---
+++ VendaLanches/Models/Lanche.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendaLanches.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public int DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool EmEstoque { get; set; }
+        public bool CategoraId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
